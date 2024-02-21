02cefabe74038acab5ad7de8bc00716d7800f6c9
venkateswara489 <157685969+venkateswara489@users.noreply.github.com>
2024-02-21 16:41:09 +0530

    Update evaluation of postfix expression.c

--- evaluation of postfix expression.c
+++ evaluation of postfix expression.c
@@ -1,60 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-double evaluateExpression(char expression[]) {
-    char operators[100];
-    double operands[100];
-    int opCount = 0;
-    int numCount = 0;
-    for (int i = 0; expression[i] != '\0'; i++) {
-        if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/') {
-            operators[opCount++] = expression[i];
-        } else if (expression[i] >= '0' && expression[i] <= '9') {
-            double num = expression[i] - '0';
-            while (expression[i + 1] >= '0' && expression[i + 1] <= '9') {
-                num = num * 10 + (expression[i + 1] - '0');
-                i++;
-            }
-            operands[numCount++] = num;
-        }
+
+#define MAX 100
+char stack[MAX];
+int top = -1; 
+
+void push(int x) {
+    if (top == MAX - 1) {
+        printf("Stack overflow\n");
+        exit(1);
+    } else {
+        stack[++top] = x;
     }
-    double result = operands[0];
-    for (int i = 0; i < opCount; i++) {
-        switch (operators[i]) {
-            case '+':
-                result += operands[i + 1];
-                break;
-            case '-':
-                result -= operands[i + 1];
-                break;
-            case '*':
-                result *= operands[i + 1];
-                break;
-            case '/':
-                if (operands[i + 1] != 0) {
-                    result /= operands[i + 1];
-                } else {
-                    printf("Error: Division by zero\n");
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            default:
-                printf("Error: Invalid operator\n");
-                exit(EXIT_FAILURE);
-        }
-    }
-    return result;
 }
-int main() {
-    char expression[100];
-    printf("Enter an expression: ");
-    fgets(expression, sizeof(expression), stdin);
-    for (int i = 0; expression[i] != '\0'; i++) {
-        if (expression[i] == '\n') {
-            expression[i] = '\0';
-            break;
-        }
+
+int pop() {
+    if (top == -1) {
+        printf("Stack underflow\n");
+        exit(1);
+    } else {
+        return stack[top--];
     }
-    double result = evaluateExpression(expression);
-    printf("Result: %lf\n", result);
+}
+
+int evaluate(char x, int op1, int op2) {
+    switch (x) {
+        case '+': return op1 + op2;
+        case '-': return op1 - op2;
+        case '*': return op1 * op2;
+        case '/': 
+            if (op2 == 0) {
+                printf("Division by zero error\n");
+                exit(1);
+            }
+            return op1 / op2;
+    }
+    return 0; 
+}
+
+int evaluate_postfix(char postfix[]) {
+    int i = 0;
+    char x; 
+    int op1, op2;
+
+    while (postfix[i] != '\0') {
+        if (isdigit(postfix[i])) {
+            push(postfix[i] - '0');
+        } else {
+            op2 = pop();
+            op1 = pop();
+            push(evaluate(postfix[i], op1, op2));
+        }
+        i++;
+    }
+
+    return pop();
+}
+
+int main() {
+    char postfix[MAX];
+    int result;
+    printf("Enter the postfix expression: ");
+    scanf("%s", postfix);
+    result = evaluate_postfix(postfix);
+    printf("The result is: %d\n", result);
     return 0;
 }
